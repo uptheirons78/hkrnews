baadbf8f099b846159585aaa5c37ed9dbad751a6
Mauro Bono <bono.mauro@gmail.com>
2019-07-26 17:18:51 +0200

    Updates

---
+++ precache-manifest.dc224e846601c2f2870f3fe0bfdd3d5d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d16da15b3bb7e7d441a29e1d197007b",
+    "url": "/hkrnews/index.html"
+  },
+  {
+    "revision": "1d778ad1400baf002361",
+    "url": "/hkrnews/static/css/main.cdf039e1.chunk.css"
+  },
+  {
+    "revision": "85eb7abc978927ad718b",
+    "url": "/hkrnews/static/js/2.4dfffcb7.chunk.js"
+  },
+  {
+    "revision": "1d778ad1400baf002361",
+    "url": "/hkrnews/static/js/main.64b91598.chunk.js"
+  },
+  {
+    "revision": "aece17a6fab2dd46fc56",
+    "url": "/hkrnews/static/js/runtime~main.babb2e8d.js"
+  }
+]);
